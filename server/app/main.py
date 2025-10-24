fb765d40a482d3c6ed77e956dd92622078702924
Shopno Banerjee <shopnothedreamer@gmail.com>
2025-10-24 12:55:05 +0530

    fix: update CORS middleware to allow requests from https://inhalt.in

--- server/app/main.py
+++ server/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://in-halt.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://in-halt.vercel.app", "http://localhost:3000",'https://inhalt.in'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
